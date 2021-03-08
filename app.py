0ab56086608008d8a63749ae3c6564415829c7cb
Kumar Prafull <eprafull@gmail.com>
2021-03-09 01:14:12 +0530

    game

--- app.py
+++ app.py
@@ -16,10 +16,58 @@ while checker1 == 0:
     elif unit.upper() == 'L':
         weight_kg = weight * 0.45
         print(f"That's nice! did you know Mr. {color} lover that your weight in kg is {weight_lbs} kilos.")
-        checker1: 1
+        checker1 = 1
     else:
         print('Enter either K or L for Kg and lbs respectively')
 
+print(f"Hey {name} Let's play a game.")
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input(f'Guess a number({3-guess_count} attempts left): '))
+    guess_count += 1
+    if guess == secret_number:
+        print("You won!")
+        break
+else:
+    print(f'Sorry {name} but you failed.')
+
+print("Let's play another one.")
+
+print("""Following are rules of game: 
+Type any of following as many times as you want and have fun.
+start --> to start the car
+stop --> stop the car
+quit --> quit the game
+help --> seek help on how to play""")
+command = ""
+started = False
+while True:
+    command = input("Enter Your Command > ").lower()
+    if command == "start":
+        if not started:
+            print("Car started..")
+            started = True
+        else:
+            print("Car has already started")
+    elif command == "stop":
+        if started:
+            print("Car stopped..")
+            started = False
+        else:
+            print("Car has already stopped")
+    elif command == "help":
+        print("""
+start --> to start the car
+stop --> stop the car
+quit --> quit the game
+help --> seek help on how to play
+        """)
+    elif command == "quit":
+        break
+    else:
+        print("I didn't understand.")
 
 
 exercise = "Prafull's Yoga Program"
